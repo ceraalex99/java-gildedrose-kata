d620b501a8b7e84ba9cae11bd3265a950dbc008f
Alex <ceraalex99@gmail.com>
2020-12-06 21:19:42 +0100

    simplify if/else spaghetti code

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -1,9 +1,9 @@
 package tv.codely.kata.gildedrose;
 
 class GildedRose {
-    private static final String AGED_BRIE = "Aged Brie";
+    private static final String AGED_BRIE        = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final String SULFURAS         = "Sulfuras, Hand of Ragnaros";
 
     Item[] items;
 
@@ -13,53 +13,52 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
+            switch (item.name) {
+                case AGED_BRIE:
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
-                }
+                    item.sellIn = item.sellIn - 1;
+
+                    if (item.sellIn < 0) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                    break;
+                case BACKSTAGE_PASSES:
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                    item.sellIn = item.sellIn - 1;
+
+                    if (item.sellIn < 0) {
+                        item.quality = 0;
+                    }
+                    break;
+                case SULFURAS:
+                    break;
+                default:
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                    item.sellIn = item.sellIn - 1;
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
+                        }
+                    }
+                    break;
             }
         }
     }
