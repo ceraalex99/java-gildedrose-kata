873dabd0611bc4a8453b4183c4cbd9cc36a88fe2
Alex <ceraalex99@gmail.com>
2020-12-07 10:20:44 +0100

    add semantics

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -1,11 +1,11 @@
 package tv.codely.kata.gildedrose;
 
 class GildedRose {
-    private static final String AGED_BRIE = "Aged Brie";
+    private static final String AGED_BRIE        = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS         = "Sulfuras, Hand of Ragnaros";
-    private static final int MAX_QUALITY = 50;
-    private static final int MIN_QUALITY = 0;
+    private static final int    MAX_QUALITY      = 50;
+    private static final int    MIN_QUALITY      = 0;
 
     Item[] items;
 
@@ -33,31 +33,31 @@ class GildedRose {
 
     private void genericUpdateQuality(Item item) {
         if (item.quality > MIN_QUALITY) {
-            item.quality--;
+            decreaseQuality(item);
         }
-        item.sellIn--;
+        decreaseSellIn(item);
         if (item.sellIn < 0) {
             if (item.quality > MIN_QUALITY) {
-                item.quality--;
+                decreaseQuality(item);
             }
         }
     }
 
     private void backstagePassUpdateQuality(Item item) {
         if (item.quality < MAX_QUALITY) {
-            item.quality++;
+            increaseQuality(item);
         }
         if (item.sellIn < 11) {
             if (item.quality < MAX_QUALITY) {
-                item.quality++;
+                increaseQuality(item);
             }
         }
         if (item.sellIn < 6) {
             if (item.quality < MAX_QUALITY) {
-                item.quality++;
+                increaseQuality(item);
             }
         }
-        item.sellIn--;
+        decreaseSellIn(item);
 
         if (item.sellIn < 0) {
             item.quality = MIN_QUALITY;
@@ -66,14 +66,26 @@ class GildedRose {
 
     private void agedBrieUpdateQuality(Item item) {
         if (item.quality < MAX_QUALITY) {
-            item.quality++;
+            increaseQuality(item);
         }
-        item.sellIn--;
+        decreaseSellIn(item);
 
         if (item.sellIn < 0) {
             if (item.quality < MAX_QUALITY) {
-                item.quality++;
+                increaseQuality(item);
             }
         }
+    }
+
+    private void decreaseSellIn(Item item) {
+        item.sellIn--;
+    }
+
+    private void decreaseQuality(Item item) {
+        item.quality--;
+    }
+
+    private void increaseQuality(Item item) {
+        item.quality++;
     }
 }
