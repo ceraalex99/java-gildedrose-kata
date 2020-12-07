6421ae0c32ff0d1ed4418c8a199ecfc9fa76fc85
Alex <ceraalex99@gmail.com>
2020-12-07 10:28:51 +0100

    add semantics

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -5,7 +5,12 @@ class GildedRose {
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS         = "Sulfuras, Hand of Ragnaros";
     private static final int    MAX_QUALITY      = 50;
-    private static final int    MIN_QUALITY      = 0;
+    private static final int MIN_QUALITY = 0;
+    private static final int GENERIC_SELL_IN_THAT_DECREASES_QUALITY             = 0;
+    private static final int BACKSTAGE_PASS_SELL_IN_FOR_DOUBLE_INCREASE_QUALITY = 11;
+    private static final int BACKSTAGE_PASS_SELL_IN_FOR_TRIPLE_INCREASE_QUALITY = 6;
+    private static final int BACKSTAGE_PASS_SELL_IN_FOR_MIN_QUALITY = 0;
+    private static final int AGED_BRIE_SELL_IN_FOR_DOUBLE_QUALITY = 0;
 
     Item[] items;
 
@@ -17,14 +22,17 @@ class GildedRose {
         for (Item item : items) {
             switch (item.name) {
                 case AGED_BRIE:
+                    decreaseSellIn(item);
                     agedBrieUpdateQuality(item);
                     break;
                 case BACKSTAGE_PASSES:
+                    decreaseSellIn(item);
                     backstagePassUpdateQuality(item);
                     break;
                 case SULFURAS:
                     break;
                 default:
+                    decreaseSellIn(item);
                     genericUpdateQuality(item);
                     break;
             }
@@ -35,8 +43,8 @@ class GildedRose {
         if (item.quality > MIN_QUALITY) {
             decreaseQuality(item);
         }
-        decreaseSellIn(item);
-        if (item.sellIn < 0) {
+
+        if (item.sellIn < GENERIC_SELL_IN_THAT_DECREASES_QUALITY) {
             if (item.quality > MIN_QUALITY) {
                 decreaseQuality(item);
             }
@@ -47,19 +55,19 @@ class GildedRose {
         if (item.quality < MAX_QUALITY) {
             increaseQuality(item);
         }
-        if (item.sellIn < 11) {
+        if (item.sellIn < BACKSTAGE_PASS_SELL_IN_FOR_DOUBLE_INCREASE_QUALITY) {
             if (item.quality < MAX_QUALITY) {
                 increaseQuality(item);
             }
         }
-        if (item.sellIn < 6) {
+        if (item.sellIn < BACKSTAGE_PASS_SELL_IN_FOR_TRIPLE_INCREASE_QUALITY) {
             if (item.quality < MAX_QUALITY) {
                 increaseQuality(item);
             }
         }
-        decreaseSellIn(item);
 
-        if (item.sellIn < 0) {
+
+        if (item.sellIn < BACKSTAGE_PASS_SELL_IN_FOR_MIN_QUALITY) {
             item.quality = MIN_QUALITY;
         }
     }
@@ -68,9 +76,9 @@ class GildedRose {
         if (item.quality < MAX_QUALITY) {
             increaseQuality(item);
         }
-        decreaseSellIn(item);
 
-        if (item.sellIn < 0) {
+
+        if (item.sellIn < AGED_BRIE_SELL_IN_FOR_DOUBLE_QUALITY) {
             if (item.quality < MAX_QUALITY) {
                 increaseQuality(item);
             }
